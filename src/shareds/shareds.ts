3a67a00ebbf2952e1ba9ececf668534b2ba1ff33
Giorgio D'uscio <119526157+giorgioduscio@users.noreply.github.com>
2025-07-06 21:53:04 +0200

    Update shareds.ts

--- src/shareds/shareds.ts
+++ src/shareds/shareds.ts
@@ -9,16 +9,19 @@ export default class Shareds {
   navbar(){
     const navbar = document.getElementById('navbar');   
     if (navbar && !navbar.innerHTML) navbar.innerHTML = `
-      <main>
-        <details>
+      <article>
+        <details class="dropdown">
           <summary>Menu</summary>
-          <div> ${Routes.filter((item) => item.show).map((item) => `
-            <div><a href="${item.path}">${ item.path.substring(1).toUpperCase() }</a></div>
-          `).join('')}
+          <div> 
+            ${Routes.filter((item) => item.show).map((item) => `
+              <div>
+                <a href="${item.path}">${ item.path.substring(1).toUpperCase() }</a>
+              </div>
+            `).join('')}
           </div>
         </details>
         
-      </main>
+      </article>
     `;
   }
 }
